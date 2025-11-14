76fd29fafd807c7dfc13cb82ec05580596e6e2f0
ginocarlo01 <gino.carlo02@gmail.com>
2025-11-13 21:51:32 -0300

    fixed input .c

--- src/input.c
+++ src/input.c
@@ -3,8 +3,6 @@
 InputPacket read_input(void) {
     InputPacket input = {0};
 
-    // aqui você coloca exatamente o que já tinha
-    // exemplo (substitua pelo seu):
     input.up = IsKeyDown(KEY_W);
     input.down = IsKeyDown(KEY_S);
     input.left = IsKeyDown(KEY_A);
